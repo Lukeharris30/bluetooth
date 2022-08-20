263324db4c65055f690287b860d44fd8e503cd4f
lukeharris30 <8006624+Lukeharris30@users.noreply.github.com>
2022-08-19 19:07:44 -0600

    feat: show battery level

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,40 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { pausableWatch, useBluetooth } from "@vueuse/core";
 
-const { isSupported, isConnected, device, requestDevice, server } =
-  useBluetooth({
-    acceptAllDevices: true,
-    optionalServices: ["battery_service"],
-  });
+const { isSupported, isConnected, device, requestDevice } = useBluetooth({
+  acceptAllDevices: true,
+  optionalServices: ["battery_service"],
+});
 
+// Get the battery service:
+const batteryService = ref();
+const batteryLevelCharacteristic = ref();
+const batteryLevel = ref();
 const batteryPercent = ref();
+const deviceName = ref();
+
+let server = async function () {
+  console.log("setting up server");
+
+  let server = await device.value.gatt.connect();
+  console.log(server);
+  deviceName.value = server.device.gatt.device.name;
+  batteryService.value = await server.getPrimaryService("battery_service");
+  batteryLevelCharacteristic.value =
+    await batteryService.value.getCharacteristic("battery_level");
+  batteryLevel.value = await batteryLevelCharacteristic.value.readValue();
+  // Convert received buffer to number:
+  batteryPercent.value = await batteryLevel.value.getUint8(0);
+  return server;
+};
+
+watch(device, server);
 
 const isGettingBatteryLevels = ref(false);
 
 const getBatteryLevels = async () => {
   isGettingBatteryLevels.value = true;
-
-  // Get the battery service:
-  const batteryService = await server.getPrimaryService("battery_service");
 
   // Get the current battery level
   const batteryLevelCharacteristic = await batteryService.getCharacteristic(
@@ -30,11 +48,6 @@ const getBatteryLevels = async () => {
       batteryPercent.value = event.target.value.getUint8(0);
     }
   );
-
-  // Convert received buffer to number:
-  const batteryLevel = await batteryLevelCharacteristic.readValue();
-
-  batteryPercent.value = await batteryLevel.getUint8(0);
 };
 
 const { stop } = pausableWatch(isConnected, (newIsConnected) => {
@@ -49,6 +62,8 @@ const { stop } = pausableWatch(isConnected, (newIsConnected) => {
 
   <template>
   <button @click="requestDevice()">Request Bluetooth Device</button>
+  <h1>{{ deviceName }}</h1>
+  {{ batteryPercent }}%
 </template>
 
 
